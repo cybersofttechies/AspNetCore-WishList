afc4d4f23787a7fd547b55d43e685240a272df6f
cybersofttechies <97500991+cybersofttechies@users.noreply.github.com>
2022-10-07 17:51:37 -0700

    commit 8b

--- WishList/Controllers/ItemController.cs
+++ WishList/Controllers/ItemController.cs
@@ -33,12 +33,12 @@ namespace WishList.Controllers
             return RedirectToAction("Index");
         }
 
-        public IActionResult Delete(int Id)
+        public IActionResult Delete(int id)
         {
-            var item = _context.Items.FirstOrDefault(i => i.Id == Id);
+            var item = _context.Items.FirstOrDefault(i => i.Id == id);
             _context.Items.Remove(item);
             _context.SaveChanges();
-            return RedirectToAction("Index");
+            return RedirectToAction("Index", "Item");
         }
     }
 }
